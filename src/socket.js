de62bb99ffe42ca4838b5654d737415acee12347
duongnp <duongnp@ginnov.tech>
2021-12-03 09:55:59 +0700

    update websocket

--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,8 @@
 import io from "socket.io-client";
 
-// const URL = "https://test-socket-21.herokuapp.com/";
+const URL = "https://test-socket-21.herokuapp.com/";
 // const URL = "http://localhost:5000/";
-const URL = "http://164.90.130.24:5050";
+// const URL = "http://164.90.130.24:5050";
 
 const sockets = io(URL, {
   autoConnect: true,
